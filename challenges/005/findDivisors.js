4ec2ff35871a454324ed5e3a509abc7ce7cbc093
Edryl Palinis <edrylpalinis@gmail.com>
2026-09-19 22:47:58 +0800

    js: day 005 - Find the divisors!

---
+++ challenges/005/findDivisors.js
@@ -0,0 +1,15 @@
+function divisors(integer) {
+  const divisors = [];
+
+  for (let i = 2; i < integer; i++) {
+    if (integer % i == 0) {
+        divisors.push(i);
+    }
+  }
+
+  if (divisors.length == 0) {
+    return `${integer} is prime`;
+  } else {
+    return divisors;
+  }
+}
